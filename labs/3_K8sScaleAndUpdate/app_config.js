49399df1c7cbf41ce736fd5eb92ef40f3ddf4978
Extroverted-introvert <parth.tripathi17@gmail.com>
2021-08-10 23:06:50 +0000

    Updated files

--- labs/3_K8sScaleAndUpdate/app_config.js
+++ labs/3_K8sScaleAndUpdate/app_config.js
@@ -4,7 +4,7 @@ var hostname = os.hostname();
 var app = express()
 
 app.get('/', function(req, res) {
-  res.send('Hello world from ' + hostname + '! Your app is up and running!\n Great Work\nThis is from updated version' )
+  res.send('Hello world from ' + hostname + '! Your app is up and running!\n Great Work\nThis is from updated version' + process.env.MESSAGE + '\n')
 })
 app.listen(8080, function() {
   console.log('Sample app is listening on port 8080.')
